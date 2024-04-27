8e5e71b5ddd0082a8b9abdbe303c2031f10c3960
Faridekhalili <khalili.f@northeastern.edu>
2024-04-27 14:21:08 -0400

    Replace state by reference day 19.

--- day19/src/App.js
+++ day19/src/App.js
@@ -1,28 +1,31 @@
 import './App.css';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const [data, setData] = useState({});
-  const [city, setCity] = useState('Paris');
+  const cityRef = useRef('Paris');
 
   useEffect(() => {
     handleClick();
   }, [])
 
   const handleClick = () => {
-    axios(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8b056cc4938d7e9a8c51a5162df93618`)
+    axios(`https://api.openweathermap.org/data/2.5/weather?q=${cityRef.current}&appid=8b056cc4938d7e9a8c51a5162df93618`)
     .then((res) => setData(res.data))
     .catch((err) => console.log(err));
-    document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + city + "')"
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + cityRef.current + "')"
   }
 
   console.log(data)
+  const handleChange = (e) => {
+    cityRef.current = e.target.value;
+  }
 
   return (
     <div className='container'>
       <div className='input-box'>
-        <input placeholder='Enter a City...' value={city} onChange={(e) => setCity(e.target.value)}></input>
+        <input placeholder='Enter a City...' onChange={handleChange}></input>
         <button onClick={handleClick}>X</button>
       </div>
       <p className='temp'>{data.main ? Math.round(data.main.temp - 273.15) + '°C' : ''}</p>
